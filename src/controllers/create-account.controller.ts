45e0989bbf23ae5113d2962e025ee119072531ba
Mário Salvador <mariopaulobunga@gmail.com>
2026-08-13 14:33:01 +0100

    feat: implement user account creation with Prisma, Zod validation, and password hashing

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,45 @@
+import { BadRequestException, Body, Controller, HttpCode, Post, UsePipes } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { hash } from "bcrypt";
+import { z } from "zod";
+import { ZodValidationPipe } from "src/pipes/zod-validations-pipes";
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.email(),
+  password: z.string(),
+})
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>;
+
+@Controller("/accounts")
+export class CreateAccountController {
+  constructor(private prisma: PrismaService) { }
+
+  @Post()
+  @HttpCode(201)
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handle(@Body() body: CreateAccountBodySchema) {
+    const { name, email, password } = body;
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: {
+        email
+      }
+    })
+
+    if (userWithSameEmail) {
+      throw new BadRequestException("User already exists")
+    }
+
+    const passwordHash = await hash(password, 10);
+
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: passwordHash
+      }
+    })
+  }
+}
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -5,18 +5,11 @@ import { PrismaPg } from '@prisma/adapter-pg';
 import 'dotenv/config';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+export class PrismaService extends PrismaClient {
   constructor() {
-    // Use process.env directly here
-    const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL as string,
+    });
     super({ adapter });
-  }
-
-  async onModuleInit() {
-    await this.$connect();
-  }
-
-  async onModuleDestroy() {
-    await this.$disconnect();
   }
 }
